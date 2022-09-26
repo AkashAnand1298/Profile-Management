9b4b48c0e7c7167e53030de562bacc03db852502
sanjeev-rm <sanjeevraghu2050@gmail.com>
2022-09-26 18:18:46 +0530

    finished Alumni and Employee class

--- Profile Management for VITIANS/src/profileManagementForVitians/Alumni.java
+++ Profile Management for VITIANS/src/profileManagementForVitians/Alumni.java
@@ -1,5 +1,52 @@
 package profileManagementForVitians;
 
-public class Alumni {
+import java.time.LocalDate;
 
+public class Alumni extends Person
+{
+    private String alumniID;
+    private Integer yearOfGraduation;
+    private String degree;
+    private String currentResidingPlace;
+    
+    public Alumni(Name name, String gender, LocalDate dob, String email, String mobile, Address adress, String alumniID, Integer yearOfGraduation, String degree, String currentResidingPlace)
+    {
+		super(name, gender, dob, email, mobile, adress);
+		this.alumniID = alumniID;
+		this.yearOfGraduation = yearOfGraduation;
+		this.degree = degree;
+		this.currentResidingPlace = currentResidingPlace;
+	}
+    
+    public String getAlumniID() {
+        return alumniID;
+    }
+    
+    public void setAlumniID(String alumniID) {
+        this.alumniID = alumniID;
+    }
+    
+    public Integer getYearOfGraduation() {
+        return yearOfGraduation;
+    }
+    
+    public void setYearOfGraduation(Integer yearOfGraduation) {
+        this.yearOfGraduation = yearOfGraduation;
+    }
+    
+    public String getDegree() {
+        return degree;
+    }
+    
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+    
+    public String getCurrentResidingPlace() {
+        return currentResidingPlace;
+    }
+    
+    public void setCurrentResidingPlace(String currentResidingPlace) {
+        this.currentResidingPlace = currentResidingPlace;
+    }
 }
--- Profile Management for VITIANS/src/profileManagementForVitians/Employee.java
+++ Profile Management for VITIANS/src/profileManagementForVitians/Employee.java
@@ -1,5 +1,52 @@
 package profileManagementForVitians;
 
-public class Employee {
+import java.time.LocalDate;
 
+public class Employee extends Person
+{
+    private String employeeID;
+    private String qualification;
+    private String responsiblity;
+    private LocalDate dateOfJoining;
+    
+    public Employee(Name name, String gender, LocalDate dob, String email, String mobile, Address adress, String employeeID, String qualification, String responsiblity, LocalDate dateOfJoining)
+    {
+		super(name, gender, dob, email, mobile, adress);
+		this.employeeID = employeeID;
+		this.qualification = qualification;
+		this.responsiblity = responsiblity;
+		this.dateOfJoining = dateOfJoining;
+	}
+    
+    public String getEmployeeID() {
+        return employeeID;
+    }
+    
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+    
+    public String getQualification() {
+        return qualification;
+    }
+    
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+    
+    public String getResponsiblity() {
+        return responsiblity;
+    }
+    
+    public void setResponsiblity(String responsiblity) {
+        this.responsiblity = responsiblity;
+    }
+    
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+    
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
 }
